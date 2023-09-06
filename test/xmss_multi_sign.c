4c15386f60b5d1fbd026b8e8095b058d4e2da019
Mirazul-Islam <61985123+Mirazul-Islam@users.noreply.github.com>
2023-09-06 09:09:44 -0300

    Update xmss_multi_sign.c

--- test/xmss_multi_sign.c
+++ test/xmss_multi_sign.c
@@ -1,141 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <time.h>
+#include <pthread.h>
+
 #include "../xmss.h"
 #include "../params.h"
 #include "../randombytes.h"
 #include "../thpool.h"
 
-#define MAX_THREADS 16
-#define XMSS_SIGNATURES 25
+#define XMSS_VERIFICATION_THREADS_START 1
+#define XMSS_VERIFICATION_THREADS_LIMIT 16
 
+
+#ifdef XMSSMT
+#define XMSS_PARSE_OID xmssmt_parse_oid
+    #define XMSS_STR_TO_OID xmssmt_str_to_oid
+    #define XMSS_KEYPAIR xmssmt_keypair
+    #define XMSS_SIGN xmssmt_sign
+    #define XMSS_SIGN_OPEN xmssmt_sign_open
+#else
 #define XMSS_PARSE_OID xmss_parse_oid
 #define XMSS_STR_TO_OID xmss_str_to_oid
 #define XMSS_KEYPAIR xmss_keypair
 #define XMSS_SIGN xmss_sign
+#define XMSS_SIGN_OPEN xmss_sign_open
+#endif
 
+#ifndef XMSS_VARIANT
+#ifdef XMSSMT
+#define XMSS_VARIANT "XMSSMT-SHA2_20/2_256"
+#else
 #define XMSS_VARIANT "XMSS-SHA2_10_256"
+#endif
+#endif
 
-#define XMSS_MLEN_START 66891064
-#define XMSS_INCREMENT_RATIO 2
-#define XMSS_MLEN_LIMIT ((unsigned long long)1 * 1024 * 1024 * 1024) //1GB
-
-struct sign_args {
-    unsigned char *sk;
+// Define data structure to pass multiple arguments to thread
+typedef struct {
+    unsigned char *pk;
     unsigned char *sm;
-    unsigned char *m;
     unsigned long long smlen;
-    unsigned long long mlen;
-};
+    unsigned char *mout;
+    unsigned long long *mlen;
+} verify_data;
 
-void *sign_thread(void *arg)
+// Function which will be run in each thread to verify signatur
+void *verify_thread(void *arg)
 {
-    if (arg == NULL) {
-        printf("Thread argument is NULL.\n");
-        return NULL;
-    }
-    struct sign_args *sa = (struct sign_args *)arg;
-    XMSS_SIGN(sa->sk, sa->sm, &(sa->smlen), sa->m, sa->mlen);
-    return NULL;
+    verify_data *data = (verify_data *) arg;
+    XMSS_SIGN_OPEN(data->mout, data->mlen, data->sm, data->smleata->pk);
 }
 
 int main()
 {
-    xmss_params params;
-    uint32_t oid;
+    // Define different message lengths
+    unsigned long long mlen_values[] = {1024};
+    int mlen_values_count = sizeof(mlen_values)/sizeof(mlen_val0]);
 
-    if (XMSS_STR_TO_OID(&oid, XMSS_VARIANT)) {
-        printf("XMSS variant %s not recognized!\n", XMSS_VARIANT);
-        return -1;
-    }
-    XMSS_PARSE_OID(&params, oid);
+    for(int j = 0; j < mlen_values_count; j++)
+    {
+        unsigned long long int mlen = mlen_values[j];
 
-    unsigned char **pk = malloc(sizeof(char *) * MAX_THREADS);
-    unsigned char **sk = malloc(sizeof(char *) * MAX_THREADS);
-    struct sign_args sa[MAX_THREADS];
-    unsigned char *sm_arr[MAX_THREADS];
-    unsigned char *m_arr[MAX_THREADS];
+        xmss_params params;
+        uint32_t oid;
 
-    for (int i = 0; i < MAX_THREADS; i++) {
-        sk[i] = malloc(XMSS_OID_LEN + params.sk_bytes);
-        if(!sk[i]) {
-            printf("Memory allocation failed for sk.\n");
-            free(pk);
-            exit(1);
-        }
-        pk[i] = malloc(XMSS_OID_LEN + params.pk_bytes);
-
-        if(!pk[i]) {
-            printf("Memory allocation failed for pk.\n");
-            exit(1);
+        if (XMSS_STR_TO_OID(&oid, XMSS_VARIANT)) {
+#ifdef XMSSMT
+            printf("XMSSMT variant %s not recognized!\n", XMSS_ANT);
+#else
+            printf("XMSS variant %s not recognized!\n", XMSS_VAT);
+#endif
+            return -1;
         }
 
-    }
+        XMSS_PARSE_OID(&params, oid);
+        unsigned char pk[XMSS_OID_LEN + params.pk_bytes];
+        unsigned char sk[XMSS_OID_LEN + params.sk_bytes];
+        unsigned char *m = malloc(mlen);
+        unsigned char *sm = malloc(params.sig_bytes + mlen);
+        unsigned char *mout = malloc(params.sig_bytes + mlen);
+        unsigned long long smlen;
 
+        randombytes(m, mlen);
+        XMSS_KEYPAIR(pk, sk, oid);
+        XMSS_SIGN(sk, sm, &smlen, m, mlen);
 
+        for (int num_threads = XMSS_VERIFICATION_THREADS_START;_threads <=
+                                                               S_VERIFICATION_THREADS_LIMIT; num_threads++)
+        {
+            threadpool thpool = thpool_init(num_threads);  // Ialize threadpool with current number of threads
 
-    for (unsigned long long mlen = XMSS_MLEN_START; mlen <= XMSS_MLEN_LIMIT; mlen *= XMSS_INCREMENT_RATIO) {
+            printf("Current mlen: %llu, current number of threa%d\n", mlen, num_threads);
+            struct timeval start, end;
 
-        for (int num_threads = 1; num_threads <= MAX_THREADS; num_threads++) {
-            threadpool thpool = thpool_init(num_threads);
+            gettimeofday(&start, NULL);
+            verify_data verify_args[num_threads];
+            for(int i = 0; i < num_threads; i++)
+            {
+                verify_data *this_arg = &verify_args[i];
+                this_arg->pk = pk;
+                this_arg->sm = sm;
+                this_arg->smlen = smlen;
+                this_arg->mout = mout;
+                this_arg->mlen = &mlen;
 
-
-            int sig_per_thread;
-            for (int i = 0; i < num_threads; i++) {
-                XMSS_KEYPAIR(pk[i], sk[i], oid);
-
-                //Allocate memory for sm and m for each thread.
-                sm_arr[i] = malloc(params.sig_bytes + mlen);
-                m_arr[i] = malloc(mlen);
-                randombytes(m_arr[i], mlen);
-
-                sa[i].sk = sk[i];
-                sa[i].sm = sm_arr[i];
-                sa[i].m = m_arr[i];
-                sa[i].smlen = 0;
-                sa[i].mlen = mlen;
-
-
-                if(num_threads <= 8)
-                    sig_per_thread = num_threads * 8;
-                else
-                    sig_per_thread = num_threads * 4;
-
-                for (int j = 0; j < sig_per_thread; j++) {
-                    thpool_add_work(thpool, sign_thread, &sa[i]);
-                }
+                thpool_add_work(thpool, verify_thread, (void *)_arg);
             }
 
-            double totalTime = 0;
-            struct timeval t0, t1;
-            gettimeofday(&t0, NULL);
+
             thpool_wait(thpool);
-            gettimeofday(&t1, NULL);
+            gettimeofday(&end, NULL);
+
+            double elapsed = (end.tv_sec - start.tv_sec) * 1e6
+                             (end.tv_usec - start.tv_usec);
+            printf("Total time to verify: %lf us\n", elapsed);
+            double throughput = (num_threads / elapsed) * 1e6; verifications/sec
+            printf("Throughput: %lf verifications/sec\n\n", thrput);
             thpool_destroy(thpool);
-
-            totalTime += (t1.tv_sec - t0.tv_sec) * 1000000.0;
-            totalTime += t1.tv_usec - t0.tv_usec;
-            totalTime /= 1000000.0;
-
-            printf("mlen: %lluB, Thread(s): %d, Signatures per thread:"
-                   " %d, Throughput: %f signatures/second.\n",
-                   mlen, num_threads, sig_per_thread, (sig_per_thread) / totalTime);
-
-            //Free allocated memory
-            for (int i = 0; i < num_threads; i++) {
-                free(m_arr[i]);
-                free(sm_arr[i]);
-            }
         }
-    }
 
-    //Free allocated memory
-    for(int j = 0; j < MAX_THREADS; j++) {
-        free(sk[j]);
-        free(pk[j]);
+        free(m);
+        free(sm);
+        free(mout);
     }
-    free(sk);
-    free(pk);
 
     return 0;
 }
